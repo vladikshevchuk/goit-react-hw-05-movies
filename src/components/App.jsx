0d3c3a957c24f719aa9f0ff4ce2882769d15c9af
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2023-04-16 08:56:22 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="movies" element={<div>Movies</div>} />
+      </Routes>
     </div>
   );
 };
